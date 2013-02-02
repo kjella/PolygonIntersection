b1f9f208061338301ce9eff1752cd37f0482edfa
Kjell Arne Arvesen <kjell.arne@arvesen.org>
2013-02-02 11:21:51 +0100

    Added a new line that we are flip flopping through in order to remove the chance of CPU optimalization tricks

--- src/org/arvesen/math/TestPolygonIntersection.java
+++ src/org/arvesen/math/TestPolygonIntersection.java
@@ -11,21 +11,74 @@ public class TestPolygonIntersection {
 	// Format: Pairs of X,Y
 	private static int[] routeOfPolygons = new int[] { 2, 2, 8, 3, 9, 4, 6, 5, 6, 6, 5, 6, 3, 6, 2, 6, 2, 2 };
 	private static List<Point> lineAPoints = new ArrayList<Point>( 3 );
-
+	private static List<Point> lineBPoints = new ArrayList<Point>( 3 );
+	private static List<Point> currentLine = new ArrayList<Point>( 3 );
+	private static int NR_OF_ITERATIONS = 10000000;
 	/**
 	 * @param args
 	 */
 	public static void main( String[] args ) {
-
 		// Initialize line
 		lineAPoints.add( new Point( 1, 1 ) );
 		lineAPoints.add( new Point( 8, 3 ) );
 		lineAPoints.add( new Point( 9, 9 ) );
 
+		lineBPoints.add( new Point( 1, 1 ) );
+		lineBPoints.add( new Point( 5, 2 ) );
+		lineBPoints.add( new Point( 8, 2 ) );
+
+		plotChart();
+		
+		System.out.println( "Iterating " + NR_OF_ITERATIONS + " times to see if two lines are crossing a polyline" );		
+
+		long start = System.currentTimeMillis();
+		for (int i = 0; i < NR_OF_ITERATIONS; i++) {
+			currentLine.clear();
+			if ( i % 2 == 1 ) {
+				currentLine.addAll( lineAPoints );
+			} else {
+				currentLine.addAll( lineBPoints );
+			}
+			runTest();
+		}
+		long end = System.currentTimeMillis();
+
+		System.out.println( "Total execution took " + ( end - start ) + "ms" );
+
+	}
+
+	private static void runTest() {
+
+//		long start = System.currentTimeMillis();
+
+		PolygonIntersectionTool tool = new PolygonIntersectionTool( routeOfPolygons );
+
+		for (Point point : currentLine) {
+			boolean isWithinBoundary = tool.isPointInPolygon( point );
+			// System.out.println(
+			// "---------------------------------------------------------------------"
+			// );
+//			System.out.println( point.getX() + "," + point.getY() + " is within boundary? " + isWithinBoundary );
+			// System.out.println(
+			// "====================================================================="
+			// );
+		}
+
+//		long end = System.currentTimeMillis();
+
+//		System.out.println( "Execution took " + ( end - start ) + "ms" );
+	}
+
+	private static void plotChart() {
+		char plotPointInShape = '*';
+		char plotPointInLine = '\'';
+
 		int stopX = 10;
 		int stopY = 20;
 
-		System.out.println( "Printing out the grid!... X indicates points in PolyLine" );
+		System.out.println( "Printing out the grid!... " );
+		System.out.println( plotPointInShape + " indicates point in PolyShape" );
+		System.out.println( plotPointInLine + " indicates point in PolyLine" );
 		System.out
 				.println( "=========================================================================================================" );
 		for (int xPos = 0; xPos < stopX; xPos++) {
@@ -49,13 +102,13 @@ public class TestPolygonIntersection {
 				shouldPlotLine = shouldPlotLine( xPos, yPos, shouldPlotLine );
 
 				if ( shouldPlotLine ) {
-					System.out.print( " *" );
+					System.out.print( " " + plotPointInLine );
 				} else {
 					System.out.print( "  " );
 				}
 
 				if ( shouldPlot ) {
-					System.out.print( " X" );
+					System.out.print( " " + plotPointInShape );
 				} else {
 					if ( xPos >= 1 ) {
 						if ( yPos > 9 ) {
@@ -71,21 +124,6 @@ public class TestPolygonIntersection {
 
 		System.out
 				.println( "=========================================================================================================" );
-
-		long start = System.currentTimeMillis();
-		
-		PolygonIntersectionTool tool = new PolygonIntersectionTool( routeOfPolygons );
-		
-		for (Point point : lineAPoints) {
-			boolean isWithinBoundary = tool.isPointInPolygon( point );
-			System.out.println( "---------------------------------------------------------------------" );
-			System.out.println( point.getX() + "," + point.getY() + " is within boundary? " + isWithinBoundary );
-			System.out.println( "=====================================================================" );
-		}
-		
-		long end = System.currentTimeMillis();
-		
-		System.out.println("Execution took " + (end - start) + "ms");
 	}
 
 	private static boolean shouldPlotLine( int xPos, int yPos, boolean shouldPlotLine ) {
@@ -107,7 +145,5 @@ public class TestPolygonIntersection {
 		}
 		return shouldPlot;
 	}
-
-
 
 }
